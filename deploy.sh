22360545ef2c93b8b001b10ec6f6e0a6b7493c62
wuchunwei <wcw850710@gmail.com>
2021-12-31 16:25:00 +0800

    :rocket: 新增 github page 構建腳本

---
+++ deploy.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env sh
+
+# 发生错误时终止
+set -e
+
+# 构建
+npm run build
+
+# 进入构建文件夹
+#cd dist
+
+# 如果你要部署到自定义域名
+# echo 'www.example.com' > CNAME
+
+#git init
+git add dist -f
+git commit -m 'deploy'
+
+# 如果你要部署在 https://<USERNAME>.github.io/<REPO>
+#git push -f git@github.com:<USERNAME>/<REPO>.git main:gh-pages
+git subtree push --prefix dist origin gh-pages
+
+#cd -
