6bda4f41bc59f44b636d9c9096d5918b5b8e4a12
575 <61653790+wcw850710@users.noreply.github.com>
2022-01-12 10:43:44 +0800

    update

--- src/records/2022.js
+++ src/records/2022.js
@@ -116,6 +116,13 @@ export default {
 				{ id: d.regularSchedule.id, status: n, comment: null },
 				{ id: d.program.id, status: n, comment: null },
 			],
+			'11': [
+				{ id: d.sport.id, status: n, comment: null },
+				{ id: d.english.id, status: n, comment: null },
+				{ id: d.meditation.id, status: y, comment: null },
+				{ id: d.regularSchedule.id, status: n, comment: null },
+				{ id: d.program.id, status: y, comment: null },
+			],
 		},
 	},
 }
