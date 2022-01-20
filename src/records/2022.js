afcd40884fcba5a3c3c5b8f1e28a4046398e5f2b
575 <61653790+wcw850710@users.noreply.github.com>
2022-01-20 22:21:01 +0800

    Update 2022.js

--- src/records/2022.js
+++ src/records/2022.js
@@ -172,6 +172,13 @@ export default {
 				{ id: d.program.id, status: n, comment: null },
 				{ id: d.read.id, status: y, comment: null },
 			],
+			'20': [
+				{ id: d.sport.id, status: n, comment: null },
+				{ id: d.english.id, status: n, comment: null },
+				{ id: d.meditation.id, status: y, comment: null },
+				{ id: d.program.id, status: n, comment: null },
+				{ id: d.read.id, status: y, comment: null },
+			],
 		},
 	},
 }
